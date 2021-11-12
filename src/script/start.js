aba4493ee351959df38eb765a85f9de9a012bd77
Smolldo <maximqwerbich@gmail.com>
2021-11-13 01:42:39 +0200

    95%

--- src/script/start.js
+++ src/script/start.js
@@ -7,8 +7,10 @@ const a = document.querySelector('.casino');
 export const PLAY = () =>{
     if(refs.nameInp.value !== '' ){
         refs.playerNickName.textContent = refs.nameInp.value;
+        document.querySelector('.player_nick').textContent = refs.nameInp.value;
         refs.backdrop.classList.toggle('is-none');
         refs.gameField.classList.toggle('is-none');
+        document.querySelector('.side_btns').classList.toggle('is-none')
         //a.play();
       //  a.volume = 0.4;
     }
